a71f92bcf280b4d900fe14313f9a93d8ea207c4e
Piotr Krawiec <piotr.krawiec23@gmail.com>
2022-01-06 15:38:30 +0100

    Add flask app template

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+from flask import Flask, render_template
+
+load_dotenv()  # Load .env file
+
+POSTGRES_HOST = "localhost"
+POSTGRES_DB_NAME = "postgres"
+POSTGRES_USER = "postgres"
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+
+
+conn = psycopg2.connect(dbname=POSTGRES_DB_NAME,
+                        user=POSTGRES_USER,
+                        password=POSTGRES_PASSWORD,
+                        host=POSTGRES_HOST)
+curr = conn.cursor()
+app = Flask(__name__)
+
+@app.route('/status')
+def status():
+    return "I'm alive!!!"
