c29471d58a12ee3596e826c3c1e63ae3792942d1
Piotr Krawiec <piotr.krawiec23@gmail.com>
2022-01-07 13:46:53 +0100

    Add support for finishorder param

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 load_dotenv()  # Load .env file
 
@@ -21,14 +21,14 @@ app = Flask(__name__)
 def status():
     return "I'm alive!!!"
 
-@app.route('/restaurants/<id>')
-def restaurant_orders(id: int):
+@app.route('/restaurants/<order_id>')
+def restaurant_orders(order_id: int):
     """
     Handle restaurant orders
 
     This function by default lists orders that restaurant has
     and information about that restaurant.
-    If provided with `finish_order` param it also flags that
+    If provided with `finishorder` param it also flags that
     order as finished.
     """
 
@@ -36,8 +36,12 @@ def restaurant_orders(id: int):
     restaurant = {}
     cursor = conn.cursor()
 
+    finishorder = request.args.get('finishorder', type=int)
+    if finishorder is not None:
+        cursor.execute(f"CALL RESTAURANT_MAKE_ORDER_READY({finishorder})")
+
     # List of dishes to finish (order_id, dish_name, qty)
-    cursor.execute(f"SELECT * FROM RESTAURANT_LIST_ORDERS({id})")
+    cursor.execute(f"SELECT * FROM RESTAURANT_LIST_ORDERS({order_id})")
     results = cursor.fetchall()
     for dish in results:
         if dish[0] not in orders.keys():
@@ -46,11 +50,13 @@ def restaurant_orders(id: int):
             orders[dish[0]] += [(dish[1], dish[2])]
 
     # Get restaurant info
-    cursor.execute(f"SELECT * FROM RESTAURANT_INFO({id})")
+    cursor.execute(f"SELECT * FROM RESTAURANT_INFO({order_id})")
     results = cursor.fetchone()
     for i, desc in enumerate(cursor.description):
         restaurant[desc[0]] = results[i]
 
     cursor.close()
 
-    return render_template("restaurants.html", orders=orders, restaurant=restaurant)
+    return render_template("restaurants.html",
+                           orders=orders,
+                           restaurant=restaurant)
